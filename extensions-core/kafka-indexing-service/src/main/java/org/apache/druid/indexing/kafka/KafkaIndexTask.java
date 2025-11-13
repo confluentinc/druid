c0b34a0eb8cd323c94f2cd26be7f7143a390757f
Suraj Goel <sugoel@confluent.io>
2025-11-13 12:02:35 +0530

    Rename config to kafkaHeaderBasedFilterConfig

--- extensions-core/kafka-indexing-service/src/main/java/org/apache/druid/indexing/kafka/KafkaIndexTask.java
+++ extensions-core/kafka-indexing-service/src/main/java/org/apache/druid/indexing/kafka/KafkaIndexTask.java
@@ -116,7 +116,7 @@ public class KafkaIndexTask extends SeekableStreamIndexTask<KafkaTopicPartition,
               configMapper,
               kafkaIndexTaskIOConfig.getConfigOverrides(),
               kafkaIndexTaskIOConfig.isMultiTopic(),
-              kafkaIndexTaskIOConfig.getHeaderBasedFilteringConfig()
+              kafkaIndexTaskIOConfig.getHeaderBasedFilterConfig()
           );
 
       if (toolbox.getMonitorScheduler() != null) {
