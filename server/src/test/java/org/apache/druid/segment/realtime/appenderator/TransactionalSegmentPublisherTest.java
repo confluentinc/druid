6cfc52bec167584bf9753e1a18c9b46d471e12b5
Pankaj Kumar <pkumar@confluent.io>
2026-01-07 12:56:08 +0530

    Increase number of retries

--- server/src/test/java/org/apache/druid/segment/realtime/appenderator/TransactionalSegmentPublisherTest.java
+++ server/src/test/java/org/apache/druid/segment/realtime/appenderator/TransactionalSegmentPublisherTest.java
@@ -20,10 +20,13 @@
 package org.apache.druid.segment.realtime.appenderator;
 
 import org.apache.druid.indexing.overlord.SegmentPublishResult;
+import org.apache.druid.java.util.common.RetryUtils;
 import org.apache.druid.segment.SegmentSchemaMapping;
 import org.apache.druid.timeline.DataSegment;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
@@ -34,8 +37,19 @@ import java.util.function.Function;
 public class TransactionalSegmentPublisherTest
 {
   @Test(timeout = 60_000L)
-  public void testPublishSegments_retriesUpto5Times_ifFailureIsRetryable() throws IOException
+  public void testPublishSegments_retriesUpto12Times_ifFailureIsRetryable() throws IOException
   {
+    // Mock RetryUtils.awaitNextRetry to skip sleep delays for faster test execution
+    try (MockedStatic<RetryUtils> retryUtilsMock = Mockito.mockStatic(RetryUtils.class)) {
+      // Make awaitNextRetry() do nothing (skip the sleep)
+      retryUtilsMock.when(() -> RetryUtils.awaitNextRetry(
+              Mockito.any(),
+              Mockito.anyString(),
+              Mockito.anyInt(),
+              Mockito.anyInt(),
+              Mockito.anyBoolean()
+      )).thenAnswer(invocation -> null);
+
     final AtomicInteger attemptCount = new AtomicInteger(0);
     final TransactionalSegmentPublisher publisher = createPublisher(
         SegmentPublishResult.retryableFailure("this error is retryable"),
@@ -46,7 +60,7 @@ public class TransactionalSegmentPublisherTest
         SegmentPublishResult.retryableFailure("this error is retryable"),
         publisher.publishSegments(null, Set.of(), Function.identity(), null, null)
     );
-    Assert.assertEquals(6, attemptCount.get());
+    Assert.assertEquals(12, attemptCount.get());
   }
 
   @Test
